1f8e0c802db3e5e6b29e0f44b6c8ea7df7a4fb34
shoutyman <shoutywan@gmail.com>
2022-09-13 23:22:56 -0500

    removing extraneous files

--- goldefficiency.py
+++ goldefficiency.py
@@ -1,8 +1,6 @@
 #standard library imports
-from colorama import init, Fore, Back
 import configparser  # to read from config file
 import logging
-import threading #  to set up threads
 
 #local application imports
 from backend import setup  # for setup and os-related functions
